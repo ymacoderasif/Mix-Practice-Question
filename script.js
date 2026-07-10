f95aaea2969013a03c0919af01e4e27449c94372
Mohd Asif <mohdasif8x@gmail.com>
2026-07-10 19:57:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// Q1. Hello Function 
+// Ek function banao jo call hone par “Hello JavaScript” print 
+// kare. 
+// Kaise sochna hai (logic): 
+// Simple function banana hai jo call hone par message print 
+// kare 
+// Kya use karna hai: 
+// Function   
+// Console output   
+// Kya nahi use karna: 
+// loop   
+// if-else   
+// Simple roadmap: 
+// Function banao 
+// Message print karo 
+// Function call karo 
+
+
+// function hello(name){
+//     console.log("Hello Javascript");
+// }
+
+// hello();
+
+// function helloname(name){
+//     let great = prompt("Enter a name");
+//     console.log("Hello",great);
+// }
+
+// helloname();
+
+
+
+
+
+
+// function sum(num){
+//     let a = 10;
+//     let b = 20;
+//     console.log(a + b);    
+// }
+
+// sum();
+
+
+
+function evenodd(num){
+    if(num % 2 === 0){
+        console.log("Even");
+    }else{
+        console.log("Odd");
+    }
+}
+
+evenodd(20);
